822412f10b71f59bb6889b3a5997700f56e7bc76
fruitcathouse <125487584+fruitcathouse@users.noreply.github.com>
2023-02-16 00:53:15 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const SERVE = false; // 是否使用线上服务器
 
-const HOST = "https://esunr.xyz:8080/api/esunrIndex"; // 线上服务器地址，如果不使用请留空
+const HOST = ""; // 线上服务器地址，如果不使用请留空
 
-const BLOG_NAME = "我的博客"; // 个人网站名字
+const BLOG_NAME = "芒果猫"; // 个人网站名字
 
-const BLOG_URL = "https://blog.esunr.xyz"; // 个人网站链接（请填写完整链接）
+const BLOG_URL = "https://flyingmangocat"; // 个人网站链接（请填写完整链接）
 
 const GITHUB = "https://github.com/EsunR"; // 右上角 Github 图标链接，留空不显示
 
@@ -37,9 +37,9 @@ const PAGES_DATA = [
 ]; // 网站分类列表，请按照API.md中'/getPages'接口中返回的data格式填写
 
 const SLOGAN = [
-  "欢迎拜访",
-  "歡迎拜訪",
-  "Welcome, my friend!",
+  "欢迎访问",
+  "歡迎访问",
+  "Welcome!",
   "訪問へようこそ",
   "嗨，别来无恙",
   "不忘初心，一生浪漫",
